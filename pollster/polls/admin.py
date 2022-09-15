f47acac97d9fdab8eae5573d7b0ba587f66c71c3
Jason <jasonetienne1@gmail.com>
2022-09-15 19:22:26 +0100

    added html and pathing

--- pollster/polls/admin.py
+++ pollster/polls/admin.py
@@ -5,6 +5,11 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
+admin.site.site_header = "Pollster Admin"
+admin.site.site_title = "Pollster Admin Area"
+admin.site.index_title = "Welcome to the Pollster admin area"
+
+
 class ChoiceInLine(admin.TabularInline):
     model = Choice
     extra: 3
